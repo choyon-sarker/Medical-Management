da6eef0ee1ff73449cd7bfaa753d11f2818776f8
choyon-sarker <sarkerchoyon123@gmail.com>
2024-10-21 20:32:15 +0600

    added first

---
+++ app/src/main/java/com/devdroid/bottomnavigation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.devdroid.bottomnavigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devdroid.bottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler().postDelayed({
+            intent= Intent(this,Signup::class.java)
+            startActivity(intent)
+        },2000)
+
+    }
+}
